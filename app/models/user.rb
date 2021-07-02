6ecdec6083effa2ca0a6ba3765fad40ab04a0203
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-07-01 19:13:47 -0500

    create user and clown controllers, add jwt and bcrypt gems

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :clown_users
   has_many :clowns, through: :clown_users
+
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
 end
